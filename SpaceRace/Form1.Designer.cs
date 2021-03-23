b61ee5412ff3f626f52b9617809949de47bad3a3
Bilal Zeineddine <bilazein302@ed.amdsb.ca>
2021-03-23 12:39:55 -0400

    Add project files.

---
+++ SpaceRace/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace SpaceRace
+{
+    partial class spaceRace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.score1 = new System.Windows.Forms.Label();
+            this.score2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(187, 19);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(261, 55);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "The Great Space Race";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // subLabel
+            // 
+            this.subLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subLabel.ForeColor = System.Drawing.Color.YellowGreen;
+            this.subLabel.Location = new System.Drawing.Point(104, 48);
+            this.subLabel.Name = "subLabel";
+            this.subLabel.Size = new System.Drawing.Size(451, 55);
+            this.subLabel.TabIndex = 1;
+            this.subLabel.Text = "Press Space To Start, ESC To Exit";
+            this.subLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // score1
+            // 
+            this.score1.BackColor = System.Drawing.Color.Transparent;
+            this.score1.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score1.ForeColor = System.Drawing.Color.White;
+            this.score1.Location = new System.Drawing.Point(76, 337);
+            this.score1.Name = "score1";
+            this.score1.Size = new System.Drawing.Size(100, 23);
+            this.score1.TabIndex = 2;
+            // 
+            // score2
+            // 
+            this.score2.BackColor = System.Drawing.Color.Transparent;
+            this.score2.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score2.ForeColor = System.Drawing.Color.White;
+            this.score2.Location = new System.Drawing.Point(444, 330);
+            this.score2.Name = "score2";
+            this.score2.Size = new System.Drawing.Size(100, 23);
+            this.score2.TabIndex = 3;
+            // 
+            // spaceRace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SpaceRace.Properties.Resources.photo_1465101162946_4377e57745c3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(613, 369);
+            this.Controls.Add(this.score2);
+            this.Controls.Add(this.score1);
+            this.Controls.Add(this.subLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.Name = "spaceRace";
+            this.Text = "Space Race";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.spaceRace_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.spaceRace_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.spaceRace_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subLabel;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label score1;
+        private System.Windows.Forms.Label score2;
+    }
+}
+
